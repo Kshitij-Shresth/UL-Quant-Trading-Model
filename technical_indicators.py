424737fec39c1a7081d01435b05a1f5ec64224c4
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-28 10:46:21 +0300

    Update technical_indicators.py
    Calculating RSI at a medium-long lookback period

--- technical_indicators.py
+++ technical_indicators.py
@@ -1,2 +1,5 @@
 #Garman-Klass-Yang-Zhang Historical Volatility
 df['garman_klass_vol'] = np.sqrt(((np.log(df['high']) - np.log(df['low']))**2)/2 - (2*np.log(2) - 1) * (np.log(df['adj close']) - np.log(df['open']))**2)
+#Realtive strength index at a lookback period of 25 
+df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=25))
+
