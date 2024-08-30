8cb4ca437f694d697af13c597d2e25fa5221adf7
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-30 11:52:34 +0300

    Update filter_liquid.py
    Monthly level data in a dataframe

--- filter_liquid.py
+++ filter_liquid.py
@@ -1,2 +1,9 @@
-df.unstack('ticker')['dollar_volume'].resample('M').mean().stack().to_frame('dollar_volume')
-#
+last_cols = [c for c in df.columns.unique(0) if c not in ['dollar_volume','volume','open', 'high','low','close']]
+aggdata = pd.concat(
+    [
+        df.unstack('ticker')['dollar_volume'].resample('M').mean().stack().to_frame('dollar_volume'),
+        df.unstack()[last_cols].resample('M').last().stack('ticker')
+    ],
+    axis=1
+).dropna()
+#Aggregating data to monthly level features in a new dataframe
