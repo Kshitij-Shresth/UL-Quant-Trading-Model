6a96e8c6dc80951e71a3bf39bb517f630f25a2c8
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-31 10:32:29 +0300

    Update RollingOLS.py
    Fama French Factors implementation

--- RollingOLS.py
+++ RollingOLS.py
@@ -1,1 +1,9 @@
-#Using fama french factors to assess risk return profile + Rolling factor betas
+#Using Fama French Factors to assess risk return profile + Rolling factor betas
+ffdata = web.DataReader("F-F_Research_Data_5_Factors_2x3", "famafrench", start="2015")[0].drop("RF", axis=1)
+ffdata.index = ffdata.index.to_timestamp()
+ffdata.index = ffdata.index.tz_localize('UTC')
+
+#Resampling monthly and normalizing the values 
+ffdata = ffdata.resample("M").last().div(100)
+ffdata.index.name = 'date'
+ffdata = ffdata.join(aggdata['return_1m']).sort_index()
