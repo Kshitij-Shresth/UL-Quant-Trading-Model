d276153b49a1afeca0ac60b689490030c7b897af
Kshitij Shresth <kshitijshresth@icloud.com>
2024-09-02 11:37:27 +0300

    Update clusters.py
    Assigning clusters

--- clusters.py
+++ clusters.py
@@ -1,1 +1,5 @@
+#Assigning clusters to form a portfolio for each new month 
+def clusters(cl):
+  cl['cluster'] = KMeans(n_clusters = 5, random_state=0, init = 'random').fit(cl).labels_
+  return cl
 
