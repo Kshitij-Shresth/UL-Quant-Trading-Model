a3ea7e5a912a3c3c14f96b151f538560bb216091
Kshitij Shresth <kshitijshresth@icloud.com>
2024-09-02 12:00:52 +0300

    Update clusters.py
    Creating clusters with rsi and atr values

--- clusters.py
+++ clusters.py
@@ -2,4 +2,21 @@
 def clusters(cl):
   cl['cluster'] = KMeans(n_clusters = 5, random_state=0, init = 'random').fit(cl).labels_
   return cl
+  
+#Filtering data by clusters
+def plot(aggdata):
+    cluster0 = aggdata[aggdata['cluster'] == 0]
+    cluster1 = aggdata[aggdata['cluster'] == 1]
+    cluster2 = aggdata[aggdata['cluster'] == 2]
+    cluster3 = aggdata[aggdata['cluster'] == 3]
+    cluster4 = aggdata[aggdata['cluster'] == 4]
+    plt.scatter(cluster0['rsi'], cluster0['atr'], color='red', label='Cluster 0')
+    plt.scatter(cluster1['rsi'], cluster1['atr'], color='blue', label='Cluster 1')
+    plt.scatter(cluster2['rsi'], cluster2['atr'], color='green', label='Cluster 2')
+    plt.scatter(cluster3['rsi'], cluster3['atr'], color='black', label='Cluster 3')
+    plt.scatter(cluster4['rsi'], cluster4['atr'], color='orange', label='Cluster 4')
+    plt.xlabel('RSI')
+    plt.ylabel('ATR')
+    plt.legend()
+    plt.grid(True)
 
