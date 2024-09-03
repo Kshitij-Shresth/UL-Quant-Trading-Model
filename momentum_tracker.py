d312bfc9b6377a9e2b8aa09b1c0e47ecef1ae8b0
Kshitij Shresth <kshitijshresth@icloud.com>
2024-09-03 12:26:03 +0300

    Update momentum_tracker.py
    Cleanup

--- momentum_tracker.py
+++ momentum_tracker.py
@@ -1,12 +1,10 @@
 #Using our clusters to find high momentum stocks over a set time period
-
 def find_highest_momentum_stocks(aggdata):
     end_date = aggdata.index.get_level_values('date').max()
     start_date = end_date - pd.DateOffset(months=3)  
-    recent_data = aggdata[(aggdata.index.get_level_values('date') >= start_date) & 
-                          (aggdata.index.get_level_values('date') <= end_date)]
+    recent_data = aggdata[(aggdata.index.get_level_values('date') >= start_date) & (aggdata.index.get_level_values('date') <= end_date)]
 
-#Calculating average RSI for each stock over the last 3 months, higher RSI -> higher momentum
+#Calculating average RSI for each stock over the last 3 months
     recent_rsi = recent_data.groupby('ticker')['rsi'].mean()
     sorted_stocks = recent_rsi.sort_values(ascending=False)
 
@@ -14,10 +12,9 @@ def find_highest_momentum_stocks(aggdata):
     top_20_cutoff = sorted_stocks.quantile(0.8)
     bottom_20_cutoff = sorted_stocks.quantile(0.2)
     risk_labels = sorted_stocks.apply(
-        lambda x: 'Green' if x >= top_20_cutoff else ('Red' if x <= bottom_20_cutoff else 'Grey')
-    )
+        lambda x: 'Green' if x >= top_20_cutoff else ('Red' if x <= bottom_20_cutoff else 'Grey'))
 
-#Merging risk labels and creating a dataframe for storing indicators
+#Merging risk labels back into the rsi dataframe and creating stick to store indicators
     recent_data = recent_data.join(risk_labels.rename('risk'), on='ticker')
     stick = pd.DataFrame({
         'stock': risk_labels.index,
@@ -37,7 +34,7 @@ def cluster_risk_groups(aggdata):
     
 #Plotting a risk cluster, color coded based on momentum 
 def plot_with_risk_clusters(aggdata):
-    colors = {'Low Risk': 'green', 'High Risk': 'red', 'Swing': 'grey'}
+    colors = {'Green': 'green', 'Red': 'red', 'Grey': 'grey'}
         for risk_category, color in colors.items():
         cluster_data = aggdata[aggdata['risk'] == risk_category]
         plt.scatter(cluster_data['rsi'], cluster_data['atr'], color=color, label=risk_category)
@@ -57,4 +54,3 @@ def sticks(aggdata):
     plt.figure(figsize=(12, 8))
     plot_with_risk_clusters(clustered_data)
     plt.show()
-stick = sticks(aggdata)
